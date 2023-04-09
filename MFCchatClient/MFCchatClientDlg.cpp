b29ac5a0f29dbddc5e076b6753ab5473aea28700
a1808011648 <2391213632@qq.com>
2023-04-09 19:17:11 +0800

    加一行打印信息

--- MFCchatClient/MFCchatClientDlg.cpp
+++ MFCchatClient/MFCchatClientDlg.cpp
@@ -65,6 +65,7 @@ BEGIN_MESSAGE_MAP(CMFCchatClientDlg, CDialogEx)
 	ON_WM_SYSCOMMAND()
 	ON_WM_PAINT()
 	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_CONNECT_BTN, &CMFCchatClientDlg::OnBnClickedConnectBtn)
 END_MESSAGE_MAP()
 
 
@@ -153,3 +154,10 @@ HCURSOR CMFCchatClientDlg::OnQueryDragIcon()
 	return static_cast<HCURSOR>(m_hIcon);
 }
 
+
+
+void CMFCchatClientDlg::OnBnClickedConnectBtn()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	TRACE("[chat]测试");
+}
