cc1cbaa9bb9577076147f61c6d19240427d7819d
a1808011648 <2391213632@qq.com>
2023-04-09 19:13:03 +0800

    上传基线代码

---
+++ MFCchatServer/MFCchatServer.h
@@ -0,0 +1,32 @@
+﻿
+// MFCchatServer.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CMFCchatServerApp:
+// 有关此类的实现，请参阅 MFCchatServer.cpp
+//
+
+class CMFCchatServerApp : public CWinApp
+{
+public:
+	CMFCchatServerApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCchatServerApp theApp;
